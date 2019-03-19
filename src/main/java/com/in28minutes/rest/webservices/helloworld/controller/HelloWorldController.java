56577c749257308aa6a04a12bc31284ee9467969
Cleberson Pauluci <pauluci.cleberson@gmail.com>
2019-03-18 22:52:13 -0300

    Request e Response usando diferentes formatos, json e xml

--- src/main/java/com/in28minutes/rest/webservices/helloworld/controller/HelloWorldController.java
+++ src/main/java/com/in28minutes/rest/webservices/helloworld/controller/HelloWorldController.java
@@ -3,6 +3,7 @@ package com.in28minutes.rest.webservices.helloworld.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
