53af46a1933fa38b9dadaf39da86535dc893809e
Cleberson Pauluci <pauluci.cleberson@gmail.com>
2019-03-19 21:22:44 -0300

    Criado versoes de endpoints para person

---
+++ src/main/java/com/in28minutes/rest/webservices/person/Name.java
@@ -0,0 +1,11 @@
+package com.in28minutes.rest.webservices.person;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Name {
+	private String firstName;
+	private String srName;
+}
---
+++ src/main/java/com/in28minutes/rest/webservices/person/PersonV2.java
@@ -0,0 +1,10 @@
+package com.in28minutes.rest.webservices.person;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class PersonV2 {
+	private Name name;
+}
